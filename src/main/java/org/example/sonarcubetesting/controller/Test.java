a67d09c510a76225d1a8ff0054441fe7b2622f5b
ravikumarkalukuri <Ravi3.kalukuri@gmail.com>
2025-07-04 17:21:42 +0530

    commit 3

--- src/main/java/org/example/sonarcubetesting/controller/Test.java
+++ src/main/java/org/example/sonarcubetesting/controller/Test.java
@@ -8,10 +8,13 @@ public class Test {
     
     @GetMapping("/users")
     public String getData() {
-        //String unusedVariable = "neverused"; // Unused local variable
+         String unusedVariable = "neverused"; // Unused local variable
         //String password = "admin1232"; // Hardcoded credentials
         return "GET request successful";
+
+
     }
+
     
     @PostMapping("/create")
     public String postData(@RequestBody String data) {
